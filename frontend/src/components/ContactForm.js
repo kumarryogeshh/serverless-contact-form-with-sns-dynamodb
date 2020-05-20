bfc1d648d98cbf5f551bd005647decc813e81eec
Yogesh <kumarryogeshh@gmail.com>
2020-05-20 14:50:36 +0530

    adding form validations

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -17,30 +17,55 @@ class ContactForm extends Component {
       response: "",
     };
   }
+
   sendContactForm = (evt) => {
     evt.preventDefault();
-
-    fetch(config.submitContactFormURL, {
-      method: "POST",
-      mode: "cors",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(this.state),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        this.setState({ response: response.message });
+    if (this.validateForm()) {
+      fetch(config.submitContactFormURL, {
+        method: "POST",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.state),
       })
-      .catch((error) => console.error("Error: ", error));
-    console.log(this.state);
-    if (this.contactform) {
-      this.contactform.reset();
-      this.setState({ name: "", email: "", message: "" });
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          this.setState({ response: response.message });
+        })
+        .catch((error) => console.error("Error: ", error));
+      console.log(this.state);
+      if (this.contactform) {
+        this.contactform.reset();
+        this.setState({ name: "", email: "", message: "" });
+      }
     }
   };
 
   onFieldsChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
+  };
+
+  validateForm = () => {
+    const { name, email, message } = this.state;
+    if (typeof name != "string" || name.length === 0) {
+      this.setState({ response: "Name cannot be blank" });
+      return false;
+    }
+
+    if (
+      typeof email != "string" ||
+      email.length === 0 ||
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)
+    ) {
+      this.setState({ response: "Please enter a valid email" });
+      return false;
+    }
+
+    if (typeof message != "string" || message.length === 0) {
+      this.setState({ response: "Please enter a message." });
+      return false;
+    }
+    return true;
   };
 
   render() {
